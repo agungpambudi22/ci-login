64142e621d96d4171b8fc76cd05ddb21e3a4166b
agungpambudi22 <wahidagungpambudi3@gmail.com>
2020-06-18 12:09:19 +0700

    menambahkan fitur login dan mambuat controller user
    Changes to be committed:
    	modified:   application/controllers/Auth.php
    	new file:   application/controllers/User.php
    	modified:   application/views/auth/login.php

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -12,10 +12,47 @@ class Auth extends CI_Controller
     public function index()
     {
         $data['title'] = 'User login';
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+        $this->form_validation->set_rules('password', 'Password', 'required|trim');
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/login');
+            $this->load->view('templates/auth_footer');
+        } else {
+            $this->_login();
+        }
+    }
 
-        $this->load->view('templates/auth_header', $data);
-        $this->load->view('auth/login');
-        $this->load->view('templates/auth_footer');
+    private function _login()
+    {
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+
+        $user = $this->db->get_where('user', ['email' => $email])->row_array();
+
+        if ($user) {
+            if ($user['is_active'] == 1) {
+                if (password_verify($password, $user['password'])) {
+                    $data = [
+                        'email' => $user['email'],
+                        'role_id' => $user['role_id']
+                    ];
+                    $this->session->set_userdata($data);
+                    redirect('user');
+                } else {
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Wrong password</div>');
+                    redirect('auth');
+                }
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">This account has not been activated</div>');
+                redirect('auth');
+            }
+        } else {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+            Email is not registered
+          </div>');
+            redirect('auth');
+        }
     }
 
     public function registration()
@@ -53,5 +90,16 @@ class Auth extends CI_Controller
           </div>');
             redirect('auth');
         }
+    }
+
+    public function logout()
+    {
+        $this->session->unset_userdata('email');
+        $this->session->unset_userdata('role_id');
+
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+        You have been logout
+      </div>');
+        redirect('auth');
     }
 }
