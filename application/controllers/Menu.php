57f518f57af5c6b13ecf94454487c7584e0e7ce2
agungpambudi22 <wahidagungpambudi3@gmail.com>
2020-06-25 11:56:37 +0700

    menyelesaikan fungsi add dan delete pada sub menu management

--- application/controllers/Menu.php
+++ application/controllers/Menu.php
@@ -55,14 +55,45 @@ class Menu extends CI_Controller
 
     public function submenu()
     {
+        $this->load->model('Menu_model', 'menu');
         $data['title'] = 'Submenu Management';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
-        $data['submenu'] = $this->db->get('user_sub_menu')->result_array();
+        $data['submenu'] = $this->menu->getSubmenu();
+        $data['menu'] = $this->menu->getMenu();
 
-        $this->load->view('templates/header', $data);
-        $this->load->view('templates/sidebar', $data);
-        $this->load->view('templates/topbar', $data);
-        $this->load->view('menu/submenu', $data);
-        $this->load->view('templates/footer');
+        $text = 'Select menu';
+
+        $this->form_validation->set_rules('title', 'Title', 'required');
+        $this->form_validation->set_rules('menu_id', 'Menu', 'required', [
+            'required' => 'You need to select a menu'
+        ]);
+        $this->form_validation->set_rules('url', 'Url', 'required');
+        $this->form_validation->set_rules('icon', 'Icon', 'required');
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('menu/submenu', $data);
+            $this->load->view('templates/footer');
+        } else {
+            $data = [
+                'menu_id' => $this->input->post('menu_id'),
+                'title' => $this->input->post('title'),
+                'url' => $this->input->post('url'),
+                'icon' => $this->input->post('icon'),
+                'is_active' => $this->input->post('is_active')
+            ];
+            $this->db->insert('user_sub_menu', $data);
+
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Submenu added successfully</div>');
+            redirect('menu/submenu');
+        }
+    }
+
+    public function deleteSubMenu($id)
+    {
+        $this->db->delete('user_sub_menu', ['id' => $id]);
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Submenu deleted successfully</div>');
+        redirect('menu/submenu');
     }
 }
--- application/views/menu/submenu.php
+++ application/views/menu/submenu.php
@@ -7,6 +7,14 @@
                     <div class="row">
                         <div class="col-lg">
 
+                            <?php if (validation_errors()) : ?>
+                                <div class="alert alert-danger" role="alert">
+                                    <?= validation_errors(); ?>
+                                </div>
+                            <?php endif; ?>
+
+                            <?= $this->session->flashdata('message'); ?>
+
                             <a href="" class="btn btn-primary addMenuButton mb-3" data-toggle="modal" data-target="#newSubmenuModal">Add new submenu</a>
 
                             <table class="table table-hover">
@@ -27,13 +35,19 @@
                                         <tr>
                                             <th scope="row"><?= $i; ?></th>
                                             <td><?= $sm['title']; ?></td>
-                                            <td><?= $sm['menu_id']; ?></td>
+                                            <td><?= $sm['menu']; ?></td>
                                             <td><?= $sm['url']; ?></td>
                                             <td><?= $sm['icon']; ?></td>
-                                            <td><?= $sm['is_active']; ?></td>
+                                            <td>
+                                                <?php if ($sm['is_active'] == 1) : ?>
+                                                    <span class="badge badge-success">active</span>
+                                                <?php else : ?>
+                                                    <span class="badge badge-secondary">inactive</span>
+                                                <?php endif; ?>
+                                            </td>
                                             <td>
                                                 <a class="badge badge-warning editMenuButton" href="" data-toggle="modal" data-target="#newMenuModal" data-id="<?= $sm['id'] ?>">edit</a>
-                                                <a class="badge badge-danger" href="<?= base_url('menu/delete/' . $sm['id']) ?>">delete</a>
+                                                <a class="badge badge-danger" href="<?= base_url('menu/deleteSubMenu/' . $sm['id']) ?>">delete</a>
                                             </td>
                                         </tr>
                                         <?php $i++; ?>
@@ -59,10 +73,28 @@
                                 </button>
                             </div>
                             <div class="modal-body">
-                                <form action="<?= base_url('menu') ?>" method="post">
+                                <form action="<?= base_url('menu/submenu') ?>" method="post">
                                     <input type="hidden" name="id" id="id">
                                     <div class="form-group">
-                                        <input type="text" class="form-control" id="menu" name="menu" placeholder="Menu name">
+                                        <input type="text" class="form-control" id="title" name="title" placeholder="Submenu title">
+                                    </div>
+                                    <div class="form-group">
+                                        <select class="form-control" id="menu_id" name="menu_id" class="menu_id">
+                                            <option value="" selected>Select menu</option>;
+                                            <?php foreach ($menu as $m) : ?>
+                                                <option value="<?= $m['id']; ?>"><?= $m['menu']; ?></option>
+                                            <?php endforeach; ?>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" id="url" name="url" placeholder="Submenu url">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" id="icon" name="icon" placeholder="Submenu icon">
+                                    </div>
+                                    <div class="form-group form-check">
+                                        <input type="checkbox" class="form-check-input" value="1" id="is_active" name="is_active" checked>
+                                        <label class="form-check-label" for="is_active">is active</label>
                                     </div>
                             </div>
                             <div class="modal-footer">
